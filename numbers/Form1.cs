763b2ba24a371ff64b6df8c778e5c8a37ce83fc9
Darcie cronk <rc0037034@student.riversidecollege.ac.uk>
2019-11-25 15:13:25 +0000

    gfdrtyjugtfrtdftfweer6

--- numbers/Form1.cs
+++ numbers/Form1.cs
@@ -15,7 +15,33 @@ namespace numbers
         public Form1()
         {
             InitializeComponent();
+            double d1 = 5e6;
+            richTextBox1.AppendText(d1.ToString() + "\n");
+            richTextBox1.AppendText(d1.ToString("N") + "\n");
+            richTextBox1.AppendText(d1.ToString("N3") + "\n");
+            richTextBox1.AppendText(d1.ToString("N0") + "\n");
+            richTextBox1.AppendText(d1.ToString("0") + "\n");
+            richTextBox1.AppendText(d1.ToString("0.0") + "\n");
+            richTextBox1.AppendText(d1.ToString("0.0E0") + "\n");
+            richTextBox1.AppendText(d1.ToString("0.00E00") + "\n");
+            double pi = 3.1415926535889793;
+                richTextBox1.AppendText(pi.ToString("N3") + "\n");
+            richTextBox1.AppendText(pi.ToString("N5") + "\n");
+            richTextBox1.AppendText(pi.ToString("N15") + "\n");
+            double c = 299792458;
+                richTextBox1.AppendText(c.ToString() + "\n");
+            richTextBox1.AppendText(c.ToString("N") + "\n");
+            richTextBox1.AppendText(c.ToString("0.000000000000000e0") + "\n");
+            richTextBox1.AppendText(c.ToString("0.00e0") + "\n");
+
+
+
+
+
+
         }
+
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
